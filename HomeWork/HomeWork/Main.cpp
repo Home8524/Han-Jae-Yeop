86ad8d41e88cd6a88b580b00ef2c85d438e14c4f
한제엽 <jeby1280@naver.com>
2021-10-29 16:28:01 +0900

    update

--- HomeWork/HomeWork/Main.cpp
+++ HomeWork/HomeWork/Main.cpp
@@ -5,30 +5,31 @@ using namespace std;
 int main(void)
 
 {
-	int Lotto[6];
-	int Random;
-	int flag = 0;
-	for (int i = 0; i < 6; i++)
-	{
-		Random = rand() % 45 + 1;
-		for (int j = 0; j < i; j++)
-		{
-			if (Lotto[j] == Random) flag = 1;
-			break;
-		}
-		if (flag == 1)
-		{
-			flag = 0;
-			i--;
-			continue;
-		}
-		else Lotto[i] = Random;
-	}
+    for (int n = 0; n < 100; ++n)
+    {
+        int Lotto[6];
+        int Random;
+        int flag = 0;
+        for (int i = 0; i < 6; i++)
+        {
+            Random = rand() % 45 + 1;
+            for (int j = 0; j < i; j++)
+            {
+                if (Lotto[j] == Random) flag = 1;
+            }
+            if (flag == 1)
+            {
+                flag = 0;
+                i--;
+            }
+            else Lotto[i] = Random;
+        }
 
-	for (int i = 0; i < 6; i++)
-		cout << Lotto[i] << " ";
 
-	cout << endl;
+        for (int i = 0; i < 6; i++)
+            cout << Lotto[i] << " ";
+        cout << endl;
+    }
 
 	return 0;
 }
